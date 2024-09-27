e64e04f48555af229e019e9a4b50c5fe3d3eeea1
Warley sc <warleyrr0819@gmail.com>
2024-09-27 19:43:29 -0300

    atualização em js
    declaração de variáveis locais

---
+++ aula01/basic-js/basiccomand01.js
@@ -0,0 +1,25 @@
+// comandos basicos de js
+
+/*
+ formas de aparecer uma escritas na tela ou no console 
+*/
+
+window.alert('hello word')
+window.console.log('olá Mundo')
+
+//************************************************ */
+
+/*
+forma de confirmação
+criará uma janela e nela será escrito o que aparecará
+*/
+
+window.confirm('top')
+/************************************************* */
+
+/*
+interação como usuario
+*/
+window.prompt("Nome")
+
+/************************************************* */
---
+++ aula01/basic-js/variaveis.js
@@ -0,0 +1,56 @@
+/*
+tipos de variaveis
+*/
+//************************************************ *****/
+
+/*                formas de declaração                 */
+
+/* 
+exite em js varias formas de declarar uma var, sendo umas delas a usando forma de "letras/palavras" EX: "var x = 1", 
+A utilização de de "$" antes de declarar o valor da var EX 'var $n = 1',
+e a utilização de "_" antes de declarar o valor da var EX 'var _x = 1'
+
+*/
+
+/*                Regras a se seguir                 */
+
+
+/*
+
+-> Não começar a declaração da variavel com numeros EX; var 1n = 10; (está errado)
+
+-> É possível usar letras ou númeroa Ex; var n1 = 10; (está certo)
+
+-> É possível a utilização de acentos esímbolos EX; var média = 6; (está certo)
+
+-> Não poe conter espaçamentos nas declarações EX; var média ponderada = 6; (está errado) ou invés disso utilize EX; var média_ponderada = 10; (está certo)
+
+-> Não utiizar palavras resevadas ou seja palavras pré-definidas da linguagens EX; var var = 10; (está errado), utlizar apenas palvaras que não está reservadas, como EX; var valor = 10;
+
+
+*/
+
+
+/*                variaveis locais                 */
+
+/* tipo numerico */
+var _x = 1.5
+var $n = 2.54
+var number = 3;
+// alert(_x);   //remover comentario para ver o código em ação
+
+/* tipo textos/strings */
+var tx_text = 1
+var $text = 2
+var text = "oló mundo";
+// alert(text);   //remover comentario para ver o código em ação
+
+/* tipo booleanos */
+var _True = true;
+var $False = false;
+var True = true;
+var False = false;
+
+alert($False);  //remover comentario para ver o código em ação  
+
+//************************************************ */
